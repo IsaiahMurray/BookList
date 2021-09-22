42e94af552d679bcb38f47f8f1319b97aa9948f6
Isaiah Murray <murray.isaiah13@gmail.com>
2021-09-22 18:38:49 -0400

    Finsihed crud and views

--- BookList/Controllers/BookController.cs
+++ BookList/Controllers/BookController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,33 @@ using System.Threading.Tasks;
 
 namespace BookList.Controllers
 {
+    [Route("api/Book")]
+    [ApiController]
     public class BookController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if(bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
         }
     }
 }
